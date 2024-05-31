57567960990e75dd21490e23523ab25c5694fae3
Oussama Dinia <oussamadinia@bndsoft.onmicrosoft.com>
2024-05-31 10:46:38 +0200

    login with auth-0 standalone + observables

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,18 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAuth0 } from '@auth0/auth0-angular';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideAuth0({
+      domain: 'oudinia.eu.auth0.com',
+      clientId: 'zarlLp8B6Epr1SDIvzOpvC7rxYydybx1',
+      authorizationParams: {
+        redirect_uri: window.location.origin,
+      },
+    }),
+  ],
 };
