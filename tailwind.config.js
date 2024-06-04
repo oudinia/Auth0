19d1a8d894e8187393513148e054055894d991ee
Oussama Dinia <oussamadinia@bndsoft.onmicrosoft.com>
2024-06-04 20:13:31 +0200

    Tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    colors: {
+      purplex11: "#AD1FEA",
+      royalblue: "#E6CB46",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
