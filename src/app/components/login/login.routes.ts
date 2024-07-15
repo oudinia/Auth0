ad8bf570878ac2d6e10acb984579b20ec4da638a
Oussama Dinia <oussamadinia@bndsoft.onmicrosoft.com>
2024-07-16 00:37:57 +0200

    theme application level-1

--- src/app/components/login/login.routes.ts
+++
@@ -1,9 +0,0 @@
-// src/app/components/login/login.routes.ts
-import { Route } from '@angular/router';
-
-export default [
-  {
-    path: '',
-    loadComponent: () => import('./login.component').then(m => m.LoginComponent)
-  }
-] as Route[];
